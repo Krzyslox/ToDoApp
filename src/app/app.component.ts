533c10cbc25de90ec0754a718a13955f0e9ac3bf
Chris <krzysiek9k@gmail.com>
2020-04-14 10:52:30 +0200

    Dodano zawartosc html i funkcjonalnosc .ts listy toDo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoList';
+  tasksList: Array<string> = [];
+  doneList: Array<string> = [];
+  task: string;
+
+  add() {
+    this.tasksList.push(this.task);
+    this.task = '';
+  }
+  done(task) {
+    this.doneList.push(task);
+    this.remove(task);
+    console.log(this.doneList);
+  }
+
+  //Przekazuję z html parametr task. W funkcji następuje przefiltrowanie tabeli tasksList. Wszystkie parametry, które są różne od task są zwracane w postaci nowej tabeli.
+  remove(task) {
+    this.tasksList = this.tasksList.filter(e => e !== task);
+  }
 }
+
